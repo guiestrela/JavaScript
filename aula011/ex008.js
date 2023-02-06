be3d5fbac377c7c10de2eef74655b2b53e24df60
guiestrela <guilhermestrela@gmail.com>
2023-02-06 17:52:01 -0300

    Create ex008.js

---
+++ aula011/ex008.js
@@ -0,0 +1,6 @@
+var vel = 60
+console.log(`A velocidado do seu carro é ${vel}Km/h`)
+if (vel > 60) { // condição simples
+    console.log(`Multa de Velocidade por estar a mais de 60Km/h`)
+}
+console.log('Dirija sempre usando o cinto de seguraça!')
