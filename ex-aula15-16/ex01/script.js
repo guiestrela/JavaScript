a7dd8d3f7439bc920d44991d6a1d42332436cd61
guiestrela <guilhermestrela@gmail.com>
2023-02-13 21:56:53 -0300

    finalizaco do exercicio

--- ex-aula15-16/ex01/script.js
+++ ex-aula15-16/ex01/script.js
@@ -25,6 +25,7 @@ function adicionar() {
         let item = document.createElement('option')
         item.text = `Valor ${num.value} adicionado`
         lista.appendChild(item)
+        res.innerHTML = ''
     }else {
         window.alert('Valor invalido ou ja se encontra na lista')
     }
@@ -33,6 +34,30 @@ function adicionar() {
 }
 
 function finalizar() {
-    
+    if (valores.length == 0) {
+        window.alert('Adcione valores antes de finalizar')
+    }else {
+        let tot = valores.length
+        let maior = valores[0]
+        let menor = valores[0]
+        let soma = 0
+        let media = 0
+        for(let pos in valores) {
+            soma += valores[pos]
+            if (valores[pos] > maior)
+                maior = valores[pos]
+            if (valores[pos < menor])
+                menor = valores[pos]
+        }
+        
+        media = soma / tot
+        res.innerHTML = ''
+        res.innerHTML += `<p>Ao todo, temos ${tot} números cadastrados.</p>`
+        res.innerHTML += `<p>O maior valor informado foi ${maior}</p>`
+        res.innerHTML += `<p>O menos valor informado foi ${menor}</p>`
+        res.innerHTML += `<p>Somando todos os valores temos o total de ${soma}</p>`
+        res.innerHTML += `<p>A media dos valores e de ${media}</p>`
+
+    }
 }
 
